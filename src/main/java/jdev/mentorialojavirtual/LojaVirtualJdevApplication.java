7c54783d548ba50a9685fba2c1dd2dbf8030806c
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-10-23 23:16:08 -0300

    Criando projeto - Loja Virtual Mentoria

---
+++ src/main/java/jdev/mentorialojavirtual/LojaVirtualJdevApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentorialojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jdev/mentorialojavirtual/LojaVirtualJdevApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.mentorialojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

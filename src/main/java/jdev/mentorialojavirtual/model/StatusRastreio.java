499aabe1cfb309516cae5d072c1965d6813f0ad4
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-11-11 21:57:06 -0300

    Criando classe VendaCompraLojaVirtual

--- src/main/java/jdev/mentorialojavirtual/model/StatusRastreio.java
+++ src/main/java/jdev/mentorialojavirtual/model/StatusRastreio.java
@@ -3,10 +3,15 @@ package jdev.mentorialojavirtual.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -29,6 +34,11 @@ public class StatusRastreio implements Serializable {
 	private String estado;
 	
 	private String status;
+	
+	@ManyToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virtual_fk"))
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
 
 	public Long getId() {
 		return id;
@@ -68,6 +78,14 @@ public class StatusRastreio implements Serializable {
 
 	public void setStatus(String status) {
 		this.status = status;
+	}
+	
+	public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+		return vendaCompraLojaVirtual;
+	}
+
+	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
 	}
 
 	@Override

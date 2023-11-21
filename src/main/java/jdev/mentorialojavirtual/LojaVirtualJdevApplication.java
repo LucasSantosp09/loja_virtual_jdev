4cc67617ef986a06dd2e7e8d9d4122db6158539a
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-11-20 22:48:39 -0300

    Configurando application.properties

--- src/main/java/jdev/mentorialojavirtual/LojaVirtualJdevApplication.java
+++ src/main/java/jdev/mentorialojavirtual/LojaVirtualJdevApplication.java
@@ -2,8 +2,16 @@ package jdev.mentorialojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "jdev.mentorialojavirtual.model")
+@ComponentScan(basePackages = {"jdev.*"})
+@EnableJpaRepositories(basePackages = {"jdev.mentorialojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualJdevApplication {
 
 	public static void main(String[] args) {
